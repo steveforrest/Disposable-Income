9414c5778c03d87a9620448e1653b865be9dea8d
steveforrest <steveforrest_1@hotmail.com>
2021-07-27 13:21:19 +0000

    initial commit1

--- run.py
+++ run.py
@@ -1,1 +1,38 @@
 # python code goes here
+
+
+"""
+Choose new entry, output disposable income or output average disposable income
+"""
+
+"""
+user inputs date
+"""
+
+"""
+checks whether date is already in worksheet or not
+"""
+
+"""
+If date is already in worksheet add columns to end of row if not create new row
+"""
+
+"""
+checks whether income or expense which inputs two new columns to the worksheet
+"""
+
+"""
+Sums all entries for each month
+"""
+
+"""
+finds disposable income by finding the difference for income and expense of comparable date
+"""
+
+"""
+finds average disposable income
+"""
+
+"""
+outputs average disposable income
+"""
